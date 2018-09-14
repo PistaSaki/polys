5709f5271c8ef53758ecd9379341e0a6738e049c
Stefan Sakalos <stefan.sakalos@tipsport.cz>
2018-09-14 16:40:56 +0200

    add more dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email = 'author@gmail.com',
     description = 'Description of my package',
     packages = find_packages(),    
-    install_requires = ["pitf", "piplot"],
+    install_requires = ["pitf", "piplot", "scipy"],
 )
